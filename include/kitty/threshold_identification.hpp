41b8ce265a679dfbf403730c988b1e854dfccd76
karim-gbaoui <75078731+karim-gbaoui@users.noreply.github.com>
2020-12-17 23:15:21 +0100

    Update threshold_identification.hpp

--- include/kitty/threshold_identification.hpp
+++ include/kitty/threshold_identification.hpp
@@ -1,305 +1,210 @@
-/* kitty: C++ truth table library
- * Copyright (C) 2017-2020  EPFL
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- */
-
-/*!
-  \file threshold_identification.hpp
-  \brief Threshold logic function identification
-
-  \author CS-472 2020 Fall students
-*/
-
 #pragma once
 
-#include <vector>
-#include <fstream>
-#include <lpsolve/lp_lib.h> /* uncomment this line to include lp_solve */
-#include "traits.hpp"
-#include "isop.hpp"
-#include "kitty.hpp"
-#include "operations.hpp"
-#include "cube.hpp"
-#include "properties.hpp"
-#include "static_truth_table.hpp"
-#include "dynamic_truth_table.hpp"
-#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+
+/*ILP_SOLVE LIBRARY*/
+#include <iostream>
+#include <cstdlib>
 #include <string>
+#include <fstream>
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <lpsolve/lp_lib.h> /* uncomment this line to include lp_solve */
+/*Include SOP representation of tt*/
+#include "isop.hpp"
+#include "traits.hpp"
+#include "operations.hpp"
+#include "properties.hpp"
+#include "implicant.hpp"
+#include "print.hpp"
+#include "cube.hpp"
+#include "constructors.hpp"
+#include "bit_operations.hpp"
 
+namespace kitty {
 
-namespace kitty
-{
-
-/*! \brief Threshold logic function identification
-
-  Given a truth table, this function determines whether it is a threshold logic function (TF)
-  and finds a linear form if it is. A Boolean function is a TF if it can be expressed as
-
-  f(x_1, ..., x_n) = \sum_{i=1}^n w_i x_i >= T
-
-  where w_i are the weight values and T is the threshold value.
-  The linear form of a TF is the vector [w_1, ..., w_n; T].
-
-  \param tt The truth table
-  \param plf Pointer to a vector that will hold a linear form of `tt` if it is a TF.
-             The linear form has `tt.num_vars()` weight values and the threshold value
-             in the end.
-  \return `true` if `tt` is a TF; `false` if `tt` is a non-TF.
-*/
-
-template<typename TT, typename = std::enable_if_t<is_complete_truth_table<TT>::value>>
-bool is_threshold( const TT& tt, std::vector<int64_t>* plf = nullptr )
-{
-  std::vector<int64_t> linear_form;
-  auto numvars = tt.num_vars();
-  TT tt0=tt;
-  for ( auto i = 0u; i < numvars; i++ )
-  {
-
-    bool neg_unate = is_negative_unate_in_i(tt,i);
-    if ( is_positive_unate_in_i(tt,i) + neg_unate == 0)
-    {
-     return false;
-    }
-    if (neg_unate){
-	flip_inplace(tt0,i);
-	}
-    
-  }
-
-    dump_lp( tt0, "thresholdfunction.lp" );
-    std::system( "lp_solve thresholdfunction.lp  > solution.txt" );	
-    std::ifstream fin( "solution.txt", std::ifstream::in );
-    
-
-    /* parsing */
-    std::string line, obj;
-    std::getline( fin, line ); /* first line is empty */
-    if (line == "This problem is infeasible"){
-	return false;
-    } 
-
-    std::getline( fin, line ); /* first line is empty */
-    std::getline( fin, obj ); /* second line is the value of objective function */
-    std::getline( fin, line ); /* third line is empty */
-    //std::getline( fin, line ); /* fourth line is useless */
-    
-    while ( std::getline( fin, line ) )
-    {
-	std::stringstream ss;
-        ss<< line ;
-	std::string temp_str;
-	int temp_int;
-	while(!ss.eof()) {
-		ss >> temp_str;
-	if(std::stringstream(temp_str) >> temp_int) { 
-	
-	 linear_form.push_back(uint64_t(temp_int));
-         }
-	temp_str = "";
-	}
-    }
-    
-   std::ofstream MyFile("filename.txt");
-  
-   for (auto i = 0 ; i<linear_form.size()-1; i++){
-	if (is_negative_unate_in_i(tt,i)){
-	linear_form[i] = -linear_form[i];
-	linear_form[linear_form.size()-1] = linear_form[linear_form.size()-1] +linear_form[i];
-		} 		
-	}
-   for (auto i = 0 ; i<linear_form.size(); i++){
-	MyFile << linear_form[i] <<std::endl;
-	}
-   MyFile.close();
-
-	
-  /* if tt is TF: */
-  /* push the weight and threshold values into `linear_form` */
-  if ( plf )
-  {
-    *plf = linear_form;
-  }
-  return true;
-  
-}
-
-template<typename TT, typename = std::enable_if_t<is_complete_truth_table<TT>::value>>
-void print_lp( const TT& tt, std::ostream& os = std::cout )
-{
-  /* the objective function */
-  os << "min: ";
-  for ( uint32_t i = 0; i < tt.num_vars(); i++ )
-  {
-    
-    os << "+ "
-       << "weight" << int(i);
-  }
-  os << " +Thresh;" << std::endl;
-
-  /* onset constraints */
-  std::vector<std::vector<uint8_t>> on_const = on_set_constraints( tt );
-  for ( auto a : on_const )
-  {
-    if (a.empty()){ os<< "+ 0 " << " => " << " Thresh;"<<std::endl;
-	}
-    else{
-    for ( auto b : a )
-    {
-      os << " +"
-         << "weight" << int(b);
-    }
-    os << ">="
-       << " Thresh;" << std::endl;
-       }
-    }
-
-  /* offset constraints */
-  std::vector<std::vector<uint8_t>> off_const = off_set_constraints( tt );
-  for ( auto a : off_const )
-  {
-    if (a.empty()){ os<< "+ 0 " << " <= " << " Thresh-1;"<<std::endl;}
-    else{
-    for ( auto b : a )
-    {
-      os << "+ " <<"weight"<<int(b);
-    }
-    os << " <= "<< " Thresh-1;" << std::endl;
-    }
-  }
-
-  /* variable type declaration */
-  os << "int ";
-  for ( uint8_t i = 0; i < tt.num_vars(); i++ )
-  {
-    os << "weight" << int(i) << ", ";
-  }
-
-  os << "Thresh"<< ";" << std::endl;
-
-}
-
-template<typename TT, typename = std::enable_if_t<is_complete_truth_table<TT>::value>>
-void dump_lp( const TT& tt, std::string const& filename = "thresholdfunction.lp" ) 
-{
-  std::ofstream fout(filename, std::ofstream::out );
-  print_lp(tt, fout);
-}
-
-
-template<typename TT, typename = std::enable_if_t<is_complete_truth_table<TT>::value>>
-std::vector<std::vector<uint8_t>> on_set_constraints( const TT& tt )
-{
-
-    std::vector<cube> on_set = isop(tt);
-    std::vector<std::vector<uint8_t>> onset_const;
-    for ( auto a : on_set )
-    {
-      std::vector<uint8_t> intermediate;
-      for ( uint8_t i = 0; i < tt.num_vars(); i++ )
-      {
-        if ( a.get_mask(i)==1 && a.get_bit(i)==1 )
-        {
-          intermediate.push_back( i );
+    template<typename TT, typename = std::enable_if_t <kitty::is_complete_truth_table<TT>::value>>
+    bool is_negative_unate_in_i(const TT &tt, uint8_t i) {
+        auto const tt0 = cofactor0(tt, i);
+        auto const tt1 = cofactor1(tt, i);
+        for (auto bit = 0; bit < (2 << (tt.num_vars() - 1)); bit++) {
+            if (get_bit(tt0, bit) >= get_bit(tt1, bit)) {
+                continue;
+            } else {
+                return false;
+            }
         }
-      }
-      onset_const.push_back( intermediate );
+        return true;
     }
-    return onset_const;
-}
 
-template<typename TT, typename = std::enable_if_t<is_complete_truth_table<TT>::value>>
-TT operator~(const TT& tt)
-{
-    TT inter = tt;
-    inter.num_vars = tt.num_vars;
-    inter.bits = ~ tt.bits;	
-    return inter;
-}
-
-
-template<typename TT, typename = std::enable_if_t<is_complete_truth_table<TT>::value>>
-std::vector<std::vector<uint8_t>> off_set_constraints( const TT& tt )
-{
-
-  std::vector<cube> off_set = isop( operator~(tt));
-  std::vector<std::vector<uint8_t>> offset_const;
-  for ( auto a : off_set )
-  {
-    std::vector<uint8_t> intermediate;
-    for ( uint8_t i = 0; i < tt.num_vars(); i++ )
-    {
-      if ( !a.get_mask(i) || (a.get_mask(i) && a.get_bit(i)) )
-      {
-        intermediate.push_back( i );
-      }
+    template<typename TT, typename = std::enable_if_t <kitty::is_complete_truth_table<TT>::value>>
+    bool is_positive_unate_in_i(const TT &tt, uint8_t i) {
+        auto const tt0 = cofactor0(tt, i);
+        auto const tt1 = cofactor1(tt, i);
+        for (auto bit = 0; bit < (2 << (tt.num_vars() - 1)); bit++) {
+            if (get_bit(tt0, bit) <= get_bit(tt1, bit)) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
-    offset_const.push_back( intermediate );
-  }
-  return offset_const;
-}
 
-template<typename TT, typename = std::enable_if_t<is_complete_truth_table<TT>::value>>
-bool is_negative_unate_in_i( const TT& tt, uint8_t i )
-{
-  auto const tt0 = cofactor0( tt, i );
-  auto const tt1 = cofactor1( tt, i );
-  for ( auto bit = 0; bit < ( 2 << ( tt.num_vars() - 1 ) ); bit++ )
-  {
-    if ( get_bit( tt0, bit ) >= get_bit( tt1, bit ) )
-    {
-      continue;
+
+    template<typename TT, typename = std::enable_if_t <kitty::is_complete_truth_table<TT>::value>>
+
+    bool is_threshold(const TT &tt, std::vector <int64_t> *plf = nullptr) {
+
+        std::vector <int64_t> linear_form;
+        std::vector <int8_t> flipp;  //to store variables where the function is negative unate
+        TT tt0 = tt;
+        for (auto i = 0u; i < tt.num_vars(); i++) {
+
+            
+            if (is_positive_unate_in_i(tt, i) + is_negative_unate_in_i(tt, i) == 0) {
+                return false;
+            }
+            if (is_negative_unate_in_i(tt, i)==1) {
+                flip_inplace(tt0, i);
+                flipp.push_back(i);
+            }
+
+        }
+
+
+       
+        
+	
+	lprec *lp;
+        int Ncol, *colno = NULL, ret = 0;
+        REAL *row = NULL;
+        Ncol = tt.num_vars() + 1;
+	lp = make_lp(0, Ncol);
+        if (lp == NULL) {
+            return false; 
+        }
+
+        colno = (int *) malloc(Ncol * sizeof(*colno));
+        row = (REAL *) malloc(Ncol * sizeof(*row));
+
+        if ((colno == NULL) || (row == NULL)) {
+            return false;
+        }
+
+        if (ret == 0) {
+
+            for (auto j = 0; j <= tt.num_vars(); j++) {
+		int k=0;
+                colno[k] = j + 1;
+                row[j] = 1;
+                add_constraintex(lp, k+1, row, colno, GE, 0); 
+            }
+        }
+
+        if (ret == 0) {
+	    std::vector <kitty::cube> pos_const = isop(tt0);
+            for (auto k = 0; k < pos_const.size(); k++) {
+
+                kitty::cube pos = pos_const.at(k);
+
+                for (auto i = 0; i < tt.num_vars(); i++) {
+                colno[i] = i + 1;
+		if (pos.get_mask(i) && pos.get_bit(i))//If the variable exists and it is true
+ 		   {
+                        row[i] = 1;
+                    } else {
+                        row[i] = 0;
+                    }
+
+                }
+
+                colno[tt.num_vars()] = tt.num_vars() + 1; 
+                row[tt.num_vars()] = -1;
+		add_constraintex(lp, Ncol, row, colno, GE, 0);
+
+            }
+
+        }
+
+
+        if (ret == 0) {
+	    std::vector <kitty::cube> neg_const = isop(unary_not(tt0));
+            for (auto k = 0; k < neg_const.size(); k++) {
+		kitty::cube neg = neg_const.at(k);
+                for (auto i = 0; i < tt.num_vars(); i++) {
+                    colno[i] = i + 1;
+                     
+		if (!neg.get_mask(i) || (neg.get_mask(i) && neg.get_bit(i)))//if the variable is not in the cube or if it is in the cube and it is not in complementary form
+		   {
+                        row[i] = 1;
+                    }
+		 else {
+                        row[i] = 0;
+                    }
+                }
+                colno[tt.num_vars()] = tt.num_vars() + 1; 
+                row[tt.num_vars()] = -1;
+
+
+                add_constraintex(lp, Ncol, row, colno, LE, -1);
+
+            }
+
+        }
+
+        if (ret == 0) {
+            set_add_rowmode(lp, FALSE); /* rowmode should be turned off again when done building the model */
+            for (int j = 0; j < Ncol; j++) {
+                colno[j] = j + 1;
+                row[j] = 1;
+            }
+
+
+            set_obj_fnex(lp, Ncol, row, colno);
+
+        }
+
+
+        
+        set_minim(lp);
+	set_verbose(lp, IMPORTANT);
+	ret = solve(lp);
+
+        if (ret != 0) {
+		return false;
+        } else {
+
+	get_variables(lp, row);
+	for (auto i = 0; i < Ncol; i++) {
+                linear_form.push_back(row[i]); 
+            }
+
+            
+        for (auto i : flipp) {
+                linear_form[i] = -linear_form[i];
+                linear_form[Ncol - 1] = linear_form[Ncol - 1] + linear_form[i];
+            }
+
+
+        
+        if (row != NULL){
+                free(row);
+	}
+         if (colno != NULL){
+                free(colno);
+	}
+            if (lp != NULL){
+		/* clean up such that all used memory by lpsolve is freed */
+                delete_lp(lp);
+	}
+
+	*plf = linear_form;
+        return true;
+        }
     }
-    else
-    {
-      return false;
-    }
-  }
-  return true;
+
 }
-  template<typename TT, typename = std::enable_if_t<is_complete_truth_table<TT>::value>>
-  bool is_positive_unate_in_i( const TT& tt, uint8_t i )
-  {
-    auto const tt0 = cofactor0( tt, i );
-    auto const tt1 = cofactor1( tt, i );
-    for ( auto bit = 0; bit < ( 2 << ( tt.num_vars() - 1 ) ); bit++ )
-    {
-      if ( get_bit( tt0, bit ) <= get_bit( tt1, bit ) )
-      {
-        continue;
-      }
-      else
-      {
-        return false;
-      }
-    }
-   return true;
-  }
-  
-}
-
-
-
-
-
 
